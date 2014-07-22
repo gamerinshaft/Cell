0364e7da8721b6ac57628e499b7c920cb85f4565
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-07-22 19:41:19 +0900

    課題１の１完成

--- Sample3.java
+++ Sample3.java
@@ -1,39 +1,89 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
 class Sample3 {
-    Cell header;
+  Cell header, newCell;
+  String str;
 
-    Sample3() {
-        header = new Cell("");
-    }
+  Sample3() {
+    header = new Cell("");
+  }
 
-    void insert(Object data) {
-        Cell newCell = new Cell(data);
-        Cell pastCell;
-        pastCell = header;
-        while(pastCell.next != null){
-          pastCell = pastCell.next;
+  void insert(Object data) {
+    newCell = new Cell(data);
+    Cell pastCell = header;
+    while(isNextCellExist(pastCell)){
+      if(isNextCellDataInsertData(pastCell, data)){
+        if(isNextCellLast(pastCell)){
+          break;
+        }else{
+          pastCell.next = pastCell.next.next;
         }
-        pastCell.next = newCell;
+      }
+      pastCell = pastCell.next;
     }
+    pastCell.next = newCell;
+  }
 
-    String printList() {
-        String s = "[";
-        do{
-            s += header.data + " ";
-            header = header.next;
-        }while(header != null);
-        s += "]";
-        return s;
-    }
+  private boolean isNextCellExist(Cell cell){
+    return cell.next != null;
+  }
 
-    public static void main(String args[]) {
-        Sample3 s = new Sample3();
-        s.insert( "a" );
-        s.insert( "b" );
-        s.insert( "c" );
-        s.insert( "d" );
-        s.insert( "b" );
-        s.insert( "e" );
-        s.insert( "f" );
-        System.out.println( s.printList() );
+  private boolean isNextCellDataInsertData(Cell cell, Object data){
+    return cell.next.data.equals(data);
+  }
+
+  private boolean isNextCellLast(Cell cell){
+    return cell.next.next == null;
+  }
+
+  String printList() {
+    Cell cell = header;
+    str = "[";
+    invertInsert(cell);
+    str += "]";
+    return str;
+  }
+
+  void regularInsert(Cell cell){
+    str += cell.data + " ";
+    if(cell.next !=null){
+      regularInsert(cell.next);
     }
+  }
+
+  void invertInsert(Cell cell){
+    if(cell.next != null){
+      invertInsert(cell.next);
+    }
+    str += " " + cell.data;
+  }
+
+  public static void main(String args[]) {
+    boolean isContinue = true;
+    String value;
+    Sample3 s = new Sample3();
+    System.out.println("終了したい場合は finish と入力して下さい。");
+    while(isContinue){
+      try{
+        BufferedReader input;
+        System.out.println("input:");
+        input = new BufferedReader (new InputStreamReader (System.in));
+        value = input.readLine( );
+        if(value.equals("finish")){
+          isContinue = false;
+        }else if(value.equals("") || value.indexOf(" ") >= 0 || value.indexOf("　") >= 0){
+          System.out.println("空白文字は含まないで下さい");
+
+        }else{
+          s.insert(value);
+          System.out.println( s.printList() );
+        }
+      }catch(IOException e){
+        e.printStackTrace();
+      }
+    }
+  }
+
 }
+
